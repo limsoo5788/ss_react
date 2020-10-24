b0f50743fa33cc83d8e2c2f95cff04862037cd7a
yun <sys394880@gmail.com>
2020-10-24 17:52:08 +0900

    App.js 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Headerbar from './header/headerbar';
+import Footerbar from './footer/footerbar';
+import Main from './main/main';
 
 function App() {
   return (
     <div className="App">
-      
+      <Headerbar />
+      <Main />
+      <Footerbar />
     </div>
   );
 }
