394bfd2a39d53eff1905ef648bd0e13e6d522c02
yun <sys394880@gmail.com>
2020-10-24 18:38:53 +0900

    상단바

--- src/header/headerbar.js
+++ src/header/headerbar.js
@@ -1,11 +1,31 @@
-import '../css/App.css';
+import '../css/header.css';
 
 function Headerbar() {
   return (
     <div className="Headerbar">
-      
+      <div className="HeaderContents">
+        <Logo />
+        <div className="Middle"></div>
+        <Menu />
+      </div>
     </div>
   );
+}
+
+function Logo(){
+  return(
+    <div className="Logo">
+      <h1>아이콘.</h1>
+    </div>
+  )
+}
+
+function Menu(){
+  return(
+    <div className="Menu">
+      <h1>메뉴바 입니다.</h1>
+    </div>
+  )
 }
 
 export default Headerbar;
