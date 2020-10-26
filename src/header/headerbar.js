3549c5627caefa12b65d7e6ecbfde0c8931aac90
yun <sys394880@gmail.com>
2020-10-26 14:40:56 +0900

    header bar menu 외형 구현 part 1

--- src/header/headerbar.js
+++ src/header/headerbar.js
@@ -1,12 +1,15 @@
+import React, {useState} from "react";
+
 import '../css/header.css';
 
 function Headerbar() {
+  const [confirm, setConfirm] = useState(true);
   return (
     <div className="Headerbar">
       <div className="HeaderContents">
         <Logo />
         <div className="Middle"></div>
-        <Menu />
+        <Menu confirm = {confirm}/>
       </div>
     </div>
   );
@@ -20,10 +23,65 @@ function Logo(){
   )
 }
 
-function Menu(){
+function Menu({confirm}){
+  const [items, setItems] = useState(
+    [
+      {
+        key : 0,
+        img : "img_path",
+        alt : "Refresh",
+        type : null,
+      },
+      {
+        key : 1,
+        img : "img_path",
+        alt : "See",
+        type : null,
+      },
+      {
+        key : 2,
+        img : "img_path",
+        alt : "Complite",
+        type : null,
+      },
+      {
+        key : 3,
+        img : "img_path",
+        alt : "Fillter",
+        type : null,
+      },
+      {
+        key : 4,
+        img : "img_path",
+        alt : "Setting",
+        type : null,
+      },
+      {
+        key : 5,
+        img : "img_path",
+        alt : "User",
+        type : null,
+      },
+    ]
+  );
+  console.log(confirm);
   return(
     <div className="Menu">
-      <h1>메뉴바 입니다.</h1>
+      {
+        confirm ?
+        items.map((item) => (
+          <MenuItems key={item.key} img_alt={item.alt}  />
+        )          
+        ) : <div></div>
+      }
+    </div>
+  )
+}
+
+function MenuItems({img_alt}){
+  return(
+    <div className="Items">
+      <span>{img_alt}</span>
     </div>
   )
 }
