1ee613c9864f056fcc7fb1c17d4b52bb7271ffa3
yun <sys394880@gmail.com>
2020-10-26 17:06:33 +0900

    팝 오버 구현 part.2

--- src/popover/popover.js
+++ src/popover/popover.js
@@ -1,11 +1,23 @@
+import React, {useState} from "react";
+
 import '../css/popover.css';
 
 function Popover({pk}){
+    var contents = null;
+    if(pk == 3){
+        contents = <h1>Filter</h1>
+    }
+    if(pk == 4){
+        contents = <h1>Setting</h1>
+    }
+    if(pk == 5){
+        contents = <h1>User</h1>
+    }
     return(
         <div className="PopOver">
             <div className="PopOverContents">
 
-                {pk}
+                {contents}
             </div>
         </div>
     )
