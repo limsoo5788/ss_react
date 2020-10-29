d4edd7cd0199f4a96e3188c5fd24dc2d49153981
limsoo5788 <limsoo5788@gmail.com>
2020-10-29 13:01:43 +0900

    Popover API로 구현

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,8 @@
 import './css/App.css';
-import PopOver from './Popover';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 const React = require('react');
 
 function Menu(props) {
@@ -8,16 +11,55 @@ function Menu(props) {
     const [alt, setAlt] = React.useState(props.items.alt);
     const [type, setType] = React.useState(props.items.type);
 
-    const onClick = (e) => {
-        props.setPopovertype(type)
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
     };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    //오픈할지 여부를 type과 관련지으면 될듯?
+    const open = Boolean(anchorEl)&&Boolean(type);
+    const id = open ? alt : undefined;
+
+    //타입에따라 정해진 메뉴컨텐츠가 나오도록하기 위한 switch문
+    switch(type){
+        case 1:
+            console.log("테스트성공!")
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    }
+
     return (
-        <button
-            className="menuButton"
-            onClick={onClick}
-        >
-        {alt}
-        </button>
+        <>
+            <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+                {alt}
+            </Button>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Typography className="popoverContents">
+                    <a href="https://www.naver.com">가능한가?</a>
+                </Typography>
+            </Popover>
+        </>
     );
 }
 
--- src/header/Menus.js
+++ src/header/Menus.js
@@ -1,77 +1,59 @@
 import { useEffect } from 'react';
 import '../css/Headerbar.css';
 import Menu from '../Menu'
-import Popover from '../Popover';
 const React = require('react');
 
 function Menus() {
-    const [ismenipopover, setIsmenupopover] = React.useState(0)
-    const [popovertype, setPopvertype] = React.useState(0)
-    const [menus, setMenus] = React.useState(
-        [
-            {
-                key : 0,
-                img : "img_path",
-                alt : "Refresh",
-                type : 0,
-              },
-              {
-                key : 1,
-                img : "img_path",
-                alt : "See",
-                type : 0,
-              },
-              {
-                key : 2,
-                img : "img_path",
-                alt : "Complite",
-                type : 0,
-              },
-              {
-                key : 3,
-                img : "img_path",
-                alt : "Fillter",
-                type : 1,
-              },
-              {
-                key : 4,
-                img : "img_path",
-                alt : "Setting",
-                type : 2,
-              },
-              {
-                key : 5,
-                img : "img_path",
-                alt : "User",
-                type : 3,
-              },
-        ]
-    );
-    //과거값을 가져오는게 문제있는건가?
-    //http://jsfiddle.net/LPbhS/
-    //
-    useEffect(()=> {
-        let a = popovertype;
-        
-        console.log("변경전 a후"+a)
-        return()=>{
-            if(a != popovertype){
-                console.log("다르다!")
-            }
-            console.log("변경후 a전"+a)
-            console.log(popovertype)
-        }
-    }, [popovertype]);
-    return (
-        <div className="Menus">
-            {menus.map((items)=>(
-                <Menu items={items} setPopovertype={setPopvertype}/>
-            ))}
-            <div className="menuPopover">
-                {ismenipopover? <Popover/>:<></>}
-            </div>
-        </div>
-    );
+  const [menus, setMenus] = React.useState(
+    [
+      {
+        key: 0,
+        img: "img_path",
+        alt: "Refresh",
+        type: 0,
+      },
+      {
+        key: 1,
+        img: "img_path",
+        alt: "See",
+        type: 0,
+      },
+      {
+        key: 2,
+        img: "img_path",
+        alt: "Complite",
+        type: 0,
+      },
+      {
+        key: 3,
+        img: "img_path",
+        alt: "Fillter",
+        type: 1,
+      },
+      {
+        key: 4,
+        img: "img_path",
+        alt: "Setting",
+        type: 2,
+      },
+      {
+        key: 5,
+        img: "img_path",
+        alt: "User",
+        type: 3,
+      },
+    ]
+  );
+  menus.reverse();
+  //과거값을 가져오는게 문제있는건가?
+  //http://jsfiddle.net/LPbhS/
+  return (
+    <div className="Menus">
+      {menus.map((items) => (
+        <Menu items={items} />
+      ))}
+    </div>
+  );
 }
 
 export default Menus;
