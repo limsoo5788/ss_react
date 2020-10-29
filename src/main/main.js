27ed06de49696a8af22f5188338ffd8619ca2f7f
yun <sys394880@gmail.com>
2020-10-29 16:01:39 +0900

    main 컴포넌트 구형 part.1(로그인 컴포넌트)

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,55 @@
-import '../css/App.css';
+import React, {useState} from "react";
+
+import '../css/main.css';
 
 function Main(){
+    const [logined, setLogined] = useState(false);
+    const [confirm, setConfirm] = useState(false);
     return(
         <div className="Main">
+            <NonLogined />
+        </div>
+    )
+}
 
+function NonLogined(){
+    // var contents = null;
+    var contents = <LogIn />
+    return(
+        <div className="NonLogined">
+            <div className="NonLoginedContents">
+                <div className="ContentsBox">
+                    <div className="Contents">
+                        {contents}
+                    </div>
+                    <div className="IconBox">
+                        <div className="Icon">
+                            <h1>아이콘</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function LogIn(){
+    return(
+        <div className="LoginContents">
+            <h1>Log In</h1>
+            <form className="LoginForm">
+                <div className="InputBox">
+                    <input type="text" id="id" name="id" placeholder="Input Username or Email" />
+                </div>
+                <div className="InputBox">
+                    <input type="password" id="pw" name="pw" placeholder="Password" />
+                </div>
+                <input type="Submit" value="Login" />
+            </form>
+            <div className="CASBox">
+                <p>New user? <a>Create account</a> </p>
+                <p>Forgot password? <a>Click here to reset it.</a> </p>
+            </div>
         </div>
     )
 }
