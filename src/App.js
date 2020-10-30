bb40581387d30d6db9d73501de138370ce6c57f0
yun <sys394880@gmail.com>
2020-10-30 15:52:53 +0900

    로고 이미지 사이즈 수정 및 메인 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './css/App.css';
 import Hearderbar from './header/headerbar';
+import Main from "./main/main"
 
 function App() {
   return (
     <div className="App">
       <Hearderbar/>
+      <Main />
     </div>
   );
 }
