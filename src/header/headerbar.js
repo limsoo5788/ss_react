5d532b4256af134b6d67f78da06066f4f141f934
limsoo5788 <limsoo5788@gmail.com>
2020-10-30 16:30:16 +0900

    임시저장

--- src/header/headerbar.js
+++ src/header/headerbar.js
@@ -1,122 +1,121 @@
 
 import imglogo from './logo192.png';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../css/header.css';
 import Popover from '../popover/popover';
 import { Link } from 'react-router-dom';
 
-function Headerbar({confirm}) {
+function Headerbar({ confirm }) {
   return (
     <div className="Headerbar">
-
       <div className="HeaderContents">
         <Logo />
         <div className="Middle"></div>
-        <Menu confirm = {confirm}/>
+        <Menu confirm={confirm} />
       </div>
     </div>
   );
 }
 
-function Logo(){
-  return(
+function Logo() {
+  return (
     <div className="Logo">
-    <Link to="/">
-    <img src={imglogo} className="AljjamLogo" alt="logo"/>
+      <Link to="/">
+        <img src={imglogo} className="AljjamLogo" alt="logo" />
       </Link>
     </div>
   )
 }
 
-function Menu({confirm}){
+function Menu({ confirm }) {
   const [menus, setMenus] = useState(
     [
       {
-        key : 0,
-        img : "img_path",
-        alt : "Refresh",
-        type : 0,
+        key: 0,
+        img: "img_path",
+        alt: "Refresh",
+        type: 0,
       },
       {
-        key : 1,
-        img : "img_path",
-        alt : "See",
-        type : 0,
+        key: 1,
+        img: "img_path",
+        alt: "See",
+        type: 0,
       },
       {
-        key : 2,
-        img : "img_path",
-        alt : "Complite",
-        type : 0,
+        key: 2,
+        img: "img_path",
+        alt: "Complite",
+        type: 0,
       },
       {
-        key : 3,
-        img : "img_path",
-        alt : "Fillter",
-        type : 1,
+        key: 3,
+        img: "img_path",
+        alt: "Fillter",
+        type: 1,
       },
       {
-        key : 4,
-        img : "img_path",
-        alt : "Setting",
-        type : 2,
+        key: 4,
+        img: "img_path",
+        alt: "Setting",
+        type: 2,
       },
       {
-        key : 5,
-        img : "img_path",
-        alt : "User",
-        type : 3,
+        key: 5,
+        img: "img_path",
+        alt: "User",
+        type: 3,
       },
     ]
   );
 
   const [visiable, setVisiable] = useState(false);
   const [popoverkey, setPopoverkey] = useState(0);
-  
+
   const eventMenus = (items) => {
-    if(items.key === 0){
+    if (items.key === 0) {
       window.location.reload();
     }
-    else if(items.key === 1){
+    else if (items.key === 1) {
       console.log("보기 방식 변경!");
-    }else if(items.key == 2){
-      if(items.alt === 'Complite'){
+    } else if (items.key == 2) {
+      if (items.alt === 'Complite') {
         const menu = menus.slice();
         menu[2].alt = "Ing";
         setMenus(menu);
       }
-      else{
+      else {
         const menu = menus.slice();
         menu[2].alt = "Complite";
         setMenus(menu);
       }
-    }else if(items.key === 3){
-      if(visiable === false && popoverkey !== 1){
+    } else if (items.key === 3) {
+      if (visiable === false && popoverkey !== 1) {
         setVisiable(true);
         setPopoverkey(1);
-      }else if(visiable === true && popoverkey !== 1){
+      } else if (visiable === true && popoverkey !== 1) {
         setPopoverkey(1);
-      }else{
+      } else {
         setVisiable(false);
         setPopoverkey(0);
       }
-    }else if(items.key === 4){
-      if(visiable === false && popoverkey !== 2){
+    } else if (items.key === 4) {
+      if (visiable === false && popoverkey !== 2) {
         setVisiable(true);
         setPopoverkey(2);
-      }else if(visiable === true && popoverkey !== 2){
+      } else if (visiable === true && popoverkey !== 2) {
         setPopoverkey(2);
-      }else{
+      } else {
         setVisiable(false);
         setPopoverkey(0);
       }
-    }else{
-      if(visiable === false && popoverkey !== 3){
+    } else {
+      if (visiable === false && popoverkey !== 3) {
         setVisiable(true);
         setPopoverkey(3);
-      }else if(visiable === true && popoverkey !== 3){
+      } else if (visiable === true && popoverkey !== 3) {
         setPopoverkey(3);
-      }else{
+      } else {
         setVisiable(false);
         setPopoverkey(0);
       }
@@ -125,7 +124,7 @@ function Menu({confirm}){
 
 
   const eventCancle = (typeKey) => {
-    if(typeKey === 3){
+    if (typeKey === 3) {
       console.log("로그아웃 실행!");
     }
     setVisiable(false);
@@ -133,32 +132,32 @@ function Menu({confirm}){
   }
 
 
-  return(
+  return (
     <div>
       <div className="Menu">
         {
           confirm ?
-          menus.map((item) => (
-            <MenuItems key={item.key} item={item} efunc={eventMenus}  />
-          )          
-          ) : <div></div>
+            menus.map((item) => (
+              <MenuItems key={item.key} item={item} efunc={eventMenus} />
+            )
+            ) : <div></div>
         }
       </div>
       <div>
         {
-          visiable ? <Popover pk = {popoverkey} efunc={eventCancle}/> : <div></div>
+          visiable ? <Popover pk={popoverkey} efunc={eventCancle} /> : <div></div>
         }
       </div>
     </div>
-    
-    
+
+
   )
 }
 
-function MenuItems({item, efunc}){
-  return(
+function MenuItems({ item, efunc }) {
+  return (
     <a className="Items" onClick={
-      (e) =>{
+      (e) => {
         e.preventDefault();
         efunc(item);
       }
